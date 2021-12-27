5c8c4d41b41a224cadc9154fa8cd846556e345a9
BestChennn <bestchenbestchen@gmail.com>
2021-12-27 10:46:08 +0700

    Complete

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,41 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[]){
+    for(int i =0;i<N;i++ ){
+        B[0] +=  A[i];
+    }  
+        B[0]/=N;
+
+    double preSD =0;
+    for(int i=0;i<N;i++){
+        preSD += (A[i]-B[0])*(A[i]-B[0]);
+    }
+        preSD /=N;
+        B[1] = sqrt(preSD);
+        
+    double sum = 1;
+    for(int i=0;i<N;i++ ){
+        sum = sum*A[i];
+        B[2] = pow(sum,(double)1 / N);
+    }
+
+    double sum_3 =0;
+    for(int i=0;i<N;i++){
+        sum_3 = sum_3 + (double)1 / A[i];
+    }
+        B[3]= N/sum_3 ;
+
+    B[4] = A[0] , B[5] = A[0];
+    for(int i=1;i<N;i++){
+        if(A[i]>B[4]) B[4]=A[i];
+        if(A[i]<B[5]) B[5]=A[i];
+    }
+
+
+}
+        
+
+
+
